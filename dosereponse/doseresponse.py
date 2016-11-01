3d96b1200c539bda81753fc0c20fad623de4870d
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-11-01 18:16:20 +0000

    define param function within class

--- dosereponse/doseresponse.py
+++ dosereponse/doseresponse.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 class DoseResponse():
-
     """
     Dose Response measurements, fit by 4-parameter sigmoidal curve,
     curve fit optimised by non-linear least squares.
@@ -14,26 +13,29 @@ class DoseResponse():
         self.hill_slope = None
         self.ec50 = None
 
-    def __repr__(self):
-        pass
+
+    def _param_4_sigmoid(self, x, b, c, d, e):
+        """
+        4 parameter sigmoidal curve
+        Parameters
+        ----------
+        b : hill-slope
+        c : min response
+        d : max response
+        e : EC50
+        """
+        return (c+(d-c) / (1 + np.exp(b*(np.log(x)-np.log(e)))))
 
 
     def fit(self, dose, response):
+        """fit function
+        Returns:
+        --------
+        Array - [hill_slope, min, max, ec50]
+        """
         self.dose = dose
         self.response = response
-        params, cov = curve_fit(_param_4_sigmoid, dose, response)
+        params, cov = curve_fit(self._param_4_sigmoid, dose, response)
         self.hill_slope = params[0]
         self.ec50 = params[-1]
         return params
-
-def _param_4_sigmoid(x, b, c, d, e):
-    """
-    4 parameter sigmoidal curve
-    Parameters
-    ----------
-    b : hill-slope
-    c : min response
-    d : max response
-    e : EC50
-    """
-    return (c+(d-c) / (1 + np.exp(b*(np.log(x)-np.log(e)))))
